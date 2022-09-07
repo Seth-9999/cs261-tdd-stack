4a52e76c2954a8609169c90de27f3cfd6478bee3
Seth-9999 <86006195+Seth-9999@users.noreply.github.com>
2022-09-07 00:10:45 -0400

    Simple Stack Version

--- stack.py
+++ stack.py
@@ -1,7 +1,33 @@
 # Stack: A stack.
 # Your implementation should pass the tests in stack.py.
-# YOUR NAME
+# Jon Riemer
 
 class Stack:
+    """
+    LIFO data structures. First item added to structure will be last item to be removed.
+    """
+    def __init__(self):
+        """Setup stack data structure"""
+        self._structure = []
 
-    pass
+    def pop(self):
+        """Removes last element within the Stack and returns"""
+        val = self._structure[-1]
+        self._structure.pop()
+        return val
+
+    def is_empty(self):
+        """Returns True/False if no elements within stack"""
+        if len(self._structure) == 0:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        """Looks at last element in stack and returns it. Value will still be present in the Stack"""
+        val = self._structure[-1]
+        return val
+
+    def push(self, value):
+        """Add item to the top of the stack"""
+        self._structure.append(value)
